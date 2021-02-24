f5ac22d483a371bc4f2d411a5e5b149b29f604b6
Isaacmaniratnam <46041829+Isaacmaniratnam@users.noreply.github.com>
2021-02-24 12:13:13 +0530

    Update and rename coding-round-2.py to Beyblade_Winner-coding-round-2.py

--- Beyblade_Winner-coding-round-2.py
+++ Beyblade_Winner-coding-round-2.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Apr  9 13:06:41 2020
 
@@ -13,14 +12,14 @@ def main():
 	a1=[]
 	b1=[]
 	while(ntc < 1 or ntc > 100000):
-		ntc = int(input("ntc")) #enter 1 for input
+		ntc = int(input("ntc\t")) #enter 1 for input
 		#print(ntc)
 		for x in range(ntc):
 			while (sot<1 or sot>100000 or min(condi(a1,b1))<0 or max(condi(a1,b1))> 2**63-1):
-				sot = int(input("sot")) #enter size of team 11
-				a1=input("team1").split() # enter 28427 19877 30709 18527 17409 14758 20873 18458 28587 17751 21672
+				sot = int(input("sot\t")) #enter size of team 11
+				a1=input("team1\t").split() # enter 28427 19877 30709 18527 17409 14758 20873 18458 28587 17751 21672
 				a1 = sorted(list(map(int,a1)),reverse=True)
-				b1=input("team2").split() # enter 21865 26619 20213 26217 407 30558 15430 17994 31674 2019 10539
+				b1=input("team2\t").split() # enter 21865 26619 20213 26217 407 30558 15430 17994 31674 2019 10539
 				b1 = sorted(list(map(int,b1)),reverse=True)
 				c1 = []  
 			for x in a1:
@@ -33,5 +32,5 @@ def main():
 						continue
 			print(len(c1))
 			sot = 0
-			#print(c1)
+			print(c1)
 main()
